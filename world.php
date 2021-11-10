a53bbc495d2587a425dadb5050eec62714da7dba
Yackeim Folkes <yackeimf@gmail.com>
2021-11-09 22:06:34 -0500

    Edited Files

--- world.php
+++ world.php
@@ -30,10 +30,24 @@ if(isset($country)==true && isset($_GET['context'])==false){
   echo "<td>".$row['head_of_state']."</td>";
   echo "</tr>";
   }
-  // endforeach
   echo "</table>";
-}else{
-  echo 'ERROR';
-}
+  } elseif(isset($country)==true && isset($_GET['context'])==true){
+      $stmt = $conn->query("SELECT cities.name, cities.district,cities.population FROM countries INNER JOIN cities ON countries.code = cities.country_code WHERE countries.name LIKE '%$country%'");
+      $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      echo "<table>
+      <tr>
+        <th>Name</th>
+        <th>District</th>
+        <th>Population</th>
+      </tr>";
+      foreach ($results as $row){
+        echo "<tr>";
+        echo "<td>".$row['name']."</td>";
+        echo "<td>".$row['district']."</td>";
+        echo "<td>".$row['population']."</td>";
+        echo "</tr>";
+        }
+  echo "</table>";
+  } 
 ?>
 
